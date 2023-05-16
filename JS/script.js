0aadd325f2480f742f802ad45d45b2bea13f2e02
FelipeSieiro <felipedossantos0804@gmail.com>
2023-05-15 22:40:08 -0300

    comits de alteração

--- JS/script.js
+++ JS/script.js
@@ -1,52 +1,39 @@
-const fullName = 'Felipe Sieiro'
-const amount = 1200.55
-const valid = true 
-const list = ['melancia', 'banana', 'melão' , 'abacaxi']
+const weightInput = document.querySelector("#weight")
+const heightInput = document.querySelector("#height")
+const calculateButton = document.querySelector("#calculate")
+const messageElement = document.querySelector("#message")
 
-//objeto
-const obj = {
-    name : 'Felipe Sieiro',
-    fullValue : 1200.55,
-    status: true,
- // chave : valor
-} 
 
-if (1000 === '1000') {
-    console.log('Deu Bom')
-} else{
-    console.log('Deu Ruim')
-}
+let weight
+let heigth
 
-if({} === {}){
-    console.log('Objeto: Deu bom')
-} else{
-    console.log('Objeto: Deu Ruim')
-}
-
-//estrutura de repetição 
-    //inicio          //condição          //incremento
-// for (let index = 0; index < list.length; index++) {
-//     console.log(list[index]) 
-// }
-// console.log("---------------------------------------------------------------")
-// let count = 0
-// while (count < 4) {
-//     console.log(list[count])
-//     count++
-// }
-
-list.map((listaDeFruta) => {
-    console.log(listaDeFruta)
+//on change
+weightInput.addEventListener('change', (event) => {
+    weight = event.target.value
+    console.log(weight)
 })
 
-//Função
-function soma(num1 , num2) {
-    console.log(num1 + num2)
-}
+heightInput.addEventListener('change', (event) => {
+    heigth = event.target.value
+    console.log(heigth)
+})
 
-soma(10, 40)
+calculateButton.addEventListener("click", () => {
+    //peso/altura*altura
+    const imc = weight / ( heigth * heigth)
+    console.log(imc)
 
-//Arrow Function
-
-const soma2 = (num1, num2) => console.log (num1+num2)
-soma2(40, 40)
+    if(imc<18.5){
+        alert("Abaixo do peso ideal")
+    } else if(imc < 24.9){
+        alert("Peso ideal")
+    } else if(imc < 29.9){
+        alert("Sobrepeso")
+    } else if(imc < 34.9){
+        alert("Obsedidade I")
+    }else if(imc < 39.9){
+        alert("Obesidade II")
+    }else{
+        alert("Obesidade III")
+    }
+})
